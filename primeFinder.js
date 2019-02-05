8acdb6b146458c975c1e84dafec383139f1679cd
sanjayyaldandi <sanjayyeldandi123@gmail.com>
2019-02-05 20:25:35 +0530

    add primeFinder,js file

---
+++ primeFinder.js
@@ -0,0 +1,22 @@
+let nthPrime = +process.argv[2];
+let isPrime = true;
+let count = 0;
+let primeCandidate = 2;
+
+while (count < nthPrime) {
+
+    for (let index = 2; index <= Math.sqrt(primeCandidate); index++) {
+        if (primeCandidate % index == 0) {
+            isPrime = false
+        }
+    }
+
+    if (isPrime == true) {
+        count++;
+    }
+
+    isPrime = true;
+    primeCandidate++;
+}
+
+console.log(--primeCandidate);
